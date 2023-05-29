49567971c40460fe8b782ad98285ceed7b2fb8e0
arwa_alotaibi <armo03730@gmail.com>
2023-05-29 22:19:10 +0300

    Arwa commit!

---
+++ src/main/java/Cake_Store.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author armo0
+ */
+public class Cake_Store {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
